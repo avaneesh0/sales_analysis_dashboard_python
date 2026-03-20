13932e86ab00e6497bf6cb98388b0f3f6adb031c
avaneesh singh <avaneesh548376@gmail.com>
2026-03-20 17:36:19 +0530

    replace matplotlib  with plotly for better visualization

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import pandas as pd
 from src.data_loader import loadData
 from src.visualization import (sales_per_quater, 
                                sales_per_year,
-                               sales_per_product)
+                               sales_per_product,
+                               sales_per_country,
+                               sales_per_month)
 
 sales_data = loadData()
 
@@ -18,18 +20,31 @@ col1.metric("Total sales", f"${sales_data["SALES"].sum():.0f}")
 col2.metric("Total order", sales_data["QUANTITYORDERED"].sum())
 col3.metric("Average order value", f"${sales_data["SALES"].mean():.0f}")
 
+mainfig = sales_per_month(sales_data)
+st.subheader("sales per month")
+st.plotly_chart(mainfig)
+
+col4, col5 = st.columns(2)
 fig1 = sales_per_quater(sales_data)
-fig2 = sales_per_year(sales_data)
-fig3 = sales_per_product(sales_data)
-with col1:
+fig2 = sales_per_product(sales_data)
+
+with col4:
     st.subheader("sales per quater")
-    st.pyplot(fig1)
-with col2:
-    st.subheader("sales per year")
-    st.pyplot(fig2)
-with col3:
+    st.plotly_chart(fig1, use_container_width=True)
+with col5:
     st.subheader("sales by product")
-    st.pyplot(fig3)
+    st.plotly_chart(fig2, use_container_width=True)
+    
+col6, col7 = st.columns(2)
+fig3 = sales_per_country(sales_data)
+fig4 = sales_per_year(sales_data)
+
+with col6:
+    st.subheader("sales per country")
+    st.plotly_chart(fig3, use_container_width=True)
+with col7:
+    st.subheader("sales per year ")
+    st.plotly_chart(fig4, use_container_width=True)
     
 with st.expander("View data"):
     st.dataframe(sales_data)
--- src/visualization.py
+++ src/visualization.py
@@ -1,34 +1,39 @@
 import matplotlib.pyplot as plt
-# from data_loader import loadData 
+import pandas as pd
+import plotly.express as px
+# from data_loader import loadData
 
-# sales_data = loadData()
+# data = loadData()
+# print(data)
 
 # sales per quaters bar 
-def sales_per_quater(df):
-    data = df.groupby(["YEAR_ID", "QTR_ID"])["SALES"].sum()
-    label = [f"{y}-{q}" for y, q in data.index]
-    fig ,ax = plt.subplots()
-    ax.bar(label, data.values)
-    plt.xticks(rotation=45)
+def sales_per_quater(df: pd.DataFrame):
+    data = df.groupby(["YEAR_ID", "QTR_ID"])["SALES"].sum().reset_index()
+    data["year-quater"] = data["YEAR_ID"].astype(str) + "-Q" + data["QTR_ID"].astype(str)
+    fig = px.bar(data, x="year-quater", y="SALES")
     return fig
 
 # sales per years bar
-def sales_per_year(df):
-    data = df.groupby("YEAR_ID")["SALES"].sum()
-    fig ,ax = plt.subplots()
-    ax.bar(data.index, data.values)
+def sales_per_year(df: pd.DataFrame):
+    data = df.groupby("YEAR_ID")["SALES"].sum().reset_index()
+    fig = px.bar(data, x="YEAR_ID", y="SALES")
     return fig
 
 # sales per product line bar
-def sales_per_product(df):
-    data = df.groupby("PRODUCTLINE")["SALES"].sum()
-    fig ,ax = plt.subplots()
-    ax.bar(data.index, data.values)
-    return fig
-
-def sales_per_year(df):
-    data = df.groupby("YEAR_ID")["SALES"].sum()
-    fig ,ax = plt.subplots()
-    ax.bar(data.index, data.values)
+def sales_per_product(df: pd.DataFrame):
+    data = df.groupby("PRODUCTLINE")["SALES"].sum().sort_values().reset_index()
+    fig = px.bar(data, x="PRODUCTLINE", y="SALES")
     return fig
     
+# sales per country
+def sales_per_country(df: pd.DataFrame):
+    data = df.groupby("COUNTRY")["SALES"].sum().sort_values().reset_index()
+    fig = px.bar(data, x="COUNTRY", y="SALES")
+    return fig
+
+# sales per month
+def sales_per_month(df: pd.DataFrame):
+    data = df.groupby(["YEAR_ID", "MONTH_ID"])["SALES"].sum().reset_index()
+    data["year-month"] = data["YEAR_ID"].astype(str) + "-" +  data["MONTH_ID"].astype(str)
+    fig = px.line(data, x="year-month", y="SALES")
+    return fig
